9bc7c4b77b2c2da15047da9837c83b65048f06e8
unwelcome <moyapohtaxd@mail.ru>
2024-12-19 18:43:30 +0300

    Updates

---
+++ assets/WelcomePage-Cx6EbvXv.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as c}from"./index-DRx9txWm.js";const t={};function a(n,r,s,p,_,l){return c(),o("p",null,"Welcome pagee")}const m=e(t,[["render",a]]);export{m as default};
--- assets/WelcomePage-KxPbVJqY.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-DKKAvJfH.js";const o={};function t(c,r,s,a,n,_){return" Welcome Page "}const f=e(o,[["render",t]]);export{f as default};
